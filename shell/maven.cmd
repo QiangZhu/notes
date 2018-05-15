1f1e608af04a5c0e7a34ca43666de9f052234700
QiangZhu <zhuqiang@gmail.com>
2018-05-15 11:32:46 +0800

    maven.cmd

---
+++ shell/maven.cmd
@@ -0,0 +1,39 @@
+# archetype
+mvn archetype:create-from-project
+cd target/genterated-sources/archetype/
+mvn install or mvn deploy
+mvn archetype:generate 
+-DarchetypeGroupId=com.keevol.springboot.chapter5
+-DarchetypeArtifactId=currency-webapi-with-scala-archetype
+-DarchetypeVersion=0.0.1-SNAPSHOT
+-DgroupId=com.keevol.springboot
+-DartifactId=new-scala-project-name
+-Dversion=0.0.1-SNAPSHOT
+
+
+# jar
+<build>
+		<sourceDirectory>src</sourceDirectory>
+		<plugins>
+			<plugin>
+				<artifactId>maven-compiler-plugin</artifactId>
+				<version>3.7.0</version>
+				<configuration>
+					<source>1.8</source>
+					<target>1.8</target>
+					<encoding>UTF-8</encoding>
+				</configuration>
+			</plugin>
+			<plugin>
+				<artifactId>maven-jar-plugin</artifactId>
+				<configuration>
+					<archive>
+						<manifest>
+							<addClasspath>true</addClasspath>
+							<mainClass>test.TestMain</mainClass>
+						</manifest>
+					</archive>
+				</configuration>
+			</plugin>
+		</plugins>
+	</build>
