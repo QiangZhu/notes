cf5524ffa02d191c1fa617b84d1ca00b79c1e8c0
QiangZhu <zhuqiang@gmail.com>
2018-05-15 11:27:00 +0800

    add intellij-idea.cmd

---
+++ shell/intellij-idea.cmd
@@ -0,0 +1,21 @@
+#Shortcut
+--------------------------------------------------------------------------------
+auto-completion: shift+ctrl+space
+setter getter  : fn + alt + insert
+import         : alt +enter
+prompt         : ctrl + Q
+recent files   : ctrl + E
+UML            : ctrl + alt + shift + U 
+alt + insert   : serialVersionUID toString setter getter
+go to implement: ctrl + alt + B
+type hierarchy : ctrl + H 
+class usages   : ctrl + left mouse /alt + F7
+show all methods: ctrl + F12
+find implement  : ctrl + alt + B
+project structure: ctrl + shift + alt + s
+System.out.println : soutp + Enter
+Delete current line ctrl+Y
+Delete to end of word ctrl+DEL
+Delete to beginning of word ctrl+BACKSPACE
+creat new java class: ctrl+alt+insert (asus notebook ctrl+alt+fn+insert) 
+super method or implementation respectively : ctrl+U
