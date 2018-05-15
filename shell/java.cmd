a1ee36339b93073292ef28183ce9b1b7bae6ea16
QiangZhu <zhuqiang@gmail.com>
2018-05-15 11:41:43 +0800

    update java.cmd

--- shell/java.cmd
+++ shell/java.cmd
@@ -20,6 +20,67 @@ set GEMFIRE=c:\GemFire9.1.0
 set PATH=%PATH%;$GEMFIRE$\bin
 set CLASSPATH=%CLASSPTH%\lib\geode-dependencies.jar
 
+
+#start.sh
+#!/bin/bash
+SERVER_NAME=msgCenter.jar
+PIDS=`ps -f | grep java | grep $SERVER_NAME |awk '{print $2}'`
+if [ -z "$PIDS" ]; then
+    echo "ERROR: The $SERVER_NAME does not started!"
+    exit 0
+fi
+
+echo -e "Stopping the $SERVER_NAME ...\c"
+for PID in $PIDS ; do
+    kill $PID > /dev/null 2>&1
+done
+
+COUNT=0
+while [ $COUNT -lt 1 ]; do
+    echo -e ".\c"
+    sleep 1
+    COUNT=1
+    for PID in $PIDS ; do
+        PID_EXIST=`ps -f -p $PID | grep java`
+        if [ -n "$PID_EXIST" ]; then
+            COUNT=0
+            break
+        fi
+    done
+done
+
+echo "OK!"
+echo "PID: $PIDS"
+
+#stop.sh
+#!/bin/bash
+cd `dirname $0`
+DEPLOY_DIR=`pwd`
+SERVER_NAME=msgCenter.jar
+
+PIDS=`ps -f | grep java | grep $SERVER_NAME |awk '{print $2}'`
+if [ -n "$PIDS" ]; then
+    echo "ERROR: The $SERVER_NAME already started!"
+    echo "PID: $PIDS"
+    exit 1
+fi
+
+JAVA_MEM_OPTS=""
+BITS=`java -version 2>&1 | grep -i 64-bit`
+if [ -n "$BITS" ]; then
+    JAVA_MEM_OPTS=" -jar " #-Ddubbo.spring.config=classpath:META-INF/spring/sys-root.xml -Ddubbo.shutdown.hook=true" #-Xmx2g -Xms2g -Xmn256m -XX:PermSize=128m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSwe
+epGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
+else
+    JAVA_MEM_OPTS=" -jar " #-Ddubbo.spring.config=classpath:META-INF/spring/sys-root.xml -Ddubbo.shutdown.hook=true" #-Xms1g -Xmx1g -XX:PermSize=128m -XX:SurvivorRatio=2 -XX:+UseParallelGC "
+fi
+
+echo -e "Starting the $SERVER_NAME ...\c"
+nohup java $JAVA_MEM_OPTS $SERVER_NAME >/dev/null 2>&1 &
+echo "OK!"
+PIDS=`ps -f | grep java | grep "$SERVER_NAME" | awk '{print $2}'`
+echo "PID: $PIDS"
+
+
 org.apache.tomcat.jdbc.pool.ConnectionPool
 /**
      * Convert an exception into a String
