7820dc86d7fcee72a49037771b5a2dbe0f0b79dd
QiangZhu <zhuqiang@gmail.com>
2018-05-23 11:20:04 +0800

    update apache-zookeeper.xmind,zookeeper.ref,zookeeper.cmd

--- shell/zookeeper.cmd
+++ shell/zookeeper.cmd
@@ -1,30 +1,54 @@
 # start a server
 
+    ```
     ./zkServer.sh start
+    ```
     
 # run in the background
 
+    ```
     ./zkServer.sh start-foreground
+    ```
     
 # run a clinet
 
+    ```
     ./zkCli.sh -server 192.168.145.108:2181
+    ```
     
-    java.io.IOException: Packet len13379956 is out of range!
-    <a href="https://stackoverflow.com/questions/10249579/zookeeper-cli-failing-ioexception-packet-len12343123123-is-out-of-range?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa">zookeeper packet is out of range</a>
+    java.io.IOException: Packet len13379956 is out of range! 
+    call %JAVA% "-Dzookeeper.log.dir=%ZOO_LOG_DIR%" \ 
+                "-Dzookeeper.root.logger=%ZOO_LOG4J_PROP%" \
+                "-Djute.maxbuffer=49107800" \
+                -cp "%CLASSPATH%" %ZOOMAIN% %*
     
-    call %JAVA% "-Dzookeeper.log.dir=%ZOO_LOG_DIR%" "-Dzookeeper.root.logger=%ZOO_LOG4J_PROP%" "-Djute.maxbuffer=49107800" -cp "%CLASSPATH%" %ZOOMAIN% %*
-    
-    中的"-Djute.maxbuffer=49107800"
+   "-Djute.maxbuffer=49107800"
     
 # zkcli command line console 中的帮助命令
 
+   ```
    help
-   
+   ```
 #  create a znode called /workers
 
+    ```
     create /workers ""
+    ```
     
+#  create a emphemeral znode /master
+
+    ```
+   create -e /master "master1.example.com:2223"
+   ```
+   
  # delete z znode called /workers
  
+    ```
     delete /workers 
+    ```
+    
+ # get the metadata and data of the /master znode
+ 
+    ```
+    get /master
+    ```
